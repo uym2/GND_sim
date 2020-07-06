84aa45641634473e5536d265ce1a0880d4c260f6
Mai To Uyen <uym2@Mais-MacBook-Pro-3.local>
2020-07-06 11:17:07 -0700

    add simulations for alpha 12 and 1000

--- evolve_multi_level.sh
+++ evolve_multi_level.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 for i in $(seq 1 1 35); do 
-    for alpha in 1 6; do
+    for alpha in 1 6 12 1000; do
         echo $i $alpha
         python evolve.py `echo $i/100 | bc -l` $alpha
         python test.py mutated_a$alpha\_p$(printf "%03d" $i)
